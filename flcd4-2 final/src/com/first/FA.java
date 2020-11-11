97181fe9c1b14264482e391f98f8d2c1e08ed3b6
andreibunu <61586781+andreibunu@users.noreply.github.com>
2020-11-11 11:56:13 +0200

    Add files via upload

---
+++ flcd4-2 final/src/com/first/FA.java
@@ -0,0 +1,121 @@
+package com.first;
+
+import javafx.util.Pair;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class FA {
+    private ArrayList<String> states;
+    private ArrayList<String> alphavet;
+    private ArrayList<String> finalState;
+    private String initialState;
+    private HashMap<Pair<String, String>, String> transitions;
+
+    public FA(String fileName) {
+        this.states = new ArrayList<>();
+        this.alphavet = new ArrayList<>();
+        this.finalState = new ArrayList<>();
+        this.transitions = new HashMap<>();
+        initializeFA(fileName);
+        System.out.println(checkIfAccepted());
+        if(checkIfDeterministic()){
+            System.out.println("");
+        }
+    }
+
+    private boolean checkIfDeterministic() {
+        ArrayList<Pair<String, String>> delta= new ArrayList<>();
+        for(Pair<String, String> pair: this.transitions.keySet()){
+            String state = pair.getKey();
+            Pair<String, String> deltaPair = new Pair(state, this.transitions.get(pair));
+            if(delta.contains(deltaPair))
+                return false;
+            delta.add(deltaPair);
+        }
+        return true;
+    }
+
+    private void initializeFA(String fileName) {
+        File myObj = new File(fileName);
+        Scanner myReader = null;
+        try {
+            myReader = new Scanner(myObj);
+        } catch (FileNotFoundException e) {
+            System.out.println("No such file");
+            return;
+        }
+        String[] stateLine = myReader.nextLine().split(" ");
+        Collections.addAll(this.states, stateLine);
+
+        String[] alphaLine = myReader.nextLine().split(" ");
+        Collections.addAll(this.alphavet, alphaLine);
+        this.initialState = myReader.nextLine();
+        String[] fstates = myReader.nextLine().split(" ");
+        Collections.addAll(this.finalState, fstates);
+
+        while (myReader.hasNextLine()) {
+            String[] trans = myReader.nextLine().split(" ");
+            Pair pair = new Pair(trans[0], trans[1]);
+            this.transitions.put(pair, trans[2]);
+        }
+        myReader.close();
+    }
+
+    public ArrayList<String> getStates() {
+        return states;
+    }
+
+
+    public ArrayList<String> getAlphavet() {
+        return alphavet;
+    }
+
+
+    public String getInitialState() {
+        return initialState;
+    }
+
+
+    public ArrayList<String> getFinalState() {
+        return finalState;
+    }
+
+
+    public HashMap<Pair<String, String>, String> getTransitions() {
+        return transitions;
+    }
+
+    public boolean checkIfAccepted(){
+        ArrayList<String> toGo = new ArrayList<>();
+        toGo.add(this.initialState);
+        boolean change = true;
+        ArrayList<String> lastTogo = new ArrayList<>();
+        while(!toGo.isEmpty() && change){
+            ArrayList<String> newToGo = new ArrayList<>();
+            change = false;
+            for(int i=0;i<toGo.size();i++) {
+                for (Pair<String, String> pair : this.transitions.keySet()) {
+                    if (pair.getKey().equals(toGo.get(i)) && !pair.getKey().equals(pair.getValue())){
+                        newToGo.add(pair.getValue());
+                        change = true;
+                    }
+                }
+            }
+            lastTogo = toGo;
+            toGo = newToGo;
+        }
+        for (String go : lastTogo) {
+            if (!this.finalState.contains(go)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
